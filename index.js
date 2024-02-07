ad4348f1e65de63004f6f4ace9d0860e33bca90b
Lakindu <86775461+LakinduVirajith@users.noreply.github.com>
2024-02-08 00:59:30 +0530

    project initialization

---
+++ index.js
@@ -0,0 +1,24 @@
+require('dotenv').config()
+const path = require('path')
+const { app, BrowserWindow } = require('electron')
+
+const createWindow = () => {
+    const win = new BrowserWindow({
+      width: 1920,
+      height: 1080,
+      minWidth: 1080,
+      minHeight: 720,
+      center: true
+    })
+  
+    // open devtool if in dev env
+    if (process.env.NODE_ENV === 'development'){
+        win.webContents.openDevTools()
+    }
+
+    win.loadFile(path.join(__dirname, './renderer/index.html'))
+}
+
+app.whenReady().then(() => {
+    createWindow()
+})
